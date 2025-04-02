ece61c3ba95a2e970adf62624d4d37f2d8f7cfbc
Mark Peters <mapeters101@earthlink.net>
2025-04-02 15:27:18 -0400

    Work on lesson066 to 2:20.

---
+++ lesson066.py
@@ -0,0 +1,21 @@
+a = (1,2,3)
+print(type(a))
+a = 1,2,3
+print(type(a))
+print(a)
+a = (1)
+print(type(a))
+print(a)
+
+a = ('a')
+print(type(a))
+print(a)
+
+a = (1,)
+print(type(a))
+print(a)
+
+a = 1,
+print(type(a))
+print(a)
+
