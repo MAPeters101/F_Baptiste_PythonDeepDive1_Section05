4f3494286e508184265a7f06e7e2ef83422d0851
Mark Peters <mapeters101@earthlink.net>
2025-04-03 15:16:29 -0400

    Work on lesson068 from 17:50 to 19:50.

--- lesson068.py
+++ lesson068.py
@@ -152,6 +152,16 @@ help(set)
 print(s1.union(s2))
 print('-'*80)
 
+s1 = {1,2,3}
+s2 = {3,4,5}
+s3 = {5,6,7}
+s4 = {7,8,9}
+print(s1.union(s2).union(s3).union(s4))
+print(s1.union(s2, s3, s4))
+print([*s1, *s2, *s3, *s4])
+print({*s1, *s2, *s3, *s4})
+print('-'*80)
+
 
 
 
