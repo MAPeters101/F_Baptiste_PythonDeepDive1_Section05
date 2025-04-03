7eae0d7356ac43c9db46b006ae9df46ddc3835e8
Mark Peters <mapeters101@earthlink.net>
2025-04-03 18:14:43 -0400

    Work on lesson072 from 1:30 to 3:05.

--- lesson072.py
+++ lesson072.py
@@ -6,4 +6,17 @@ func1(1,c=3,b=2)
 func1(c=3,b=2,a=1)
 print('-'*80)
 
+def func1(a, b, *args):
+    print(a, b, args)
+
+func1(1,2,3, 4)
+print()
+
+def func1(a, b, *args, d):
+    print(a, b, args, d)
+
+#func1(1,2,3, 4, 5)
+func1(1,2,3, 4, d=5)
+print('-'*80)
+
 
