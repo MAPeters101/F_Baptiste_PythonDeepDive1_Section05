9a37480f8edade531cebd89700696cb4e721efea
Mark Peters <mapeters101@earthlink.net>
2025-04-03 16:33:10 -0400

    Work on lesson070 from 2:20 to 4:40.

--- lesson070.py
+++ lesson070.py
@@ -21,6 +21,24 @@ def func1(a, b, *args):
 
 func1(10, 20)
 func1(10, 20, 1, 2, 3)
+print('-'*80)
+
+def avg(*args):
+    print(args)
+
+avg()
+avg(10, 20)
+print()
+
+def avg(*args):
+    count = len(args)
+    total = sum(args)
+    return total/count
+
+print(avg(2, 2, 4, 4))
+#print(avg())
+print('-'*80)
+
 
 
 
