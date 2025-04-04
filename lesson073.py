2c2bc67a2c1cd09c800d28c77c6ae5f16cb0bf98
Mark Peters <mapeters101@earthlink.net>
2025-04-03 23:40:55 -0400

    Work on lesson073 from 7:25 to 9:15.

--- lesson073.py
+++ lesson073.py
@@ -10,6 +10,20 @@ def func(*args, **kwargs):
 func(1, 2, x=100, y=200)
 print('-'*80)
 
+# def func(a, b, *, **kwargs):
+#     print(a)
+#     print(b)
+#     print(kwargs)
+def func(a, b, *, d, **kwargs):
+    print(a)
+    print(b)
+    print(d)
+    print(kwargs)
+#func(1, 2, x=100, y=200, 12)
+func(1, 2, d=20, x=100, y=200)
+func(1, 2, x=100, y=200, d=20)
+print('-'*80)
+
 
 
 
