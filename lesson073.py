d994f9785267e19d505f4c4675a1dbdf0931fd85
Mark Peters <mapeters101@earthlink.net>
2025-04-03 23:37:38 -0400

    Work on lesson073 from 6:20 to 7:25.

--- lesson073.py
+++ lesson073.py
@@ -3,6 +3,13 @@ def func(**others):
 func(a=1,b=2,c=3)
 print('-'*80)
 
+def func(*args, **kwargs):
+    print(args)
+    print(kwargs)
+#func(1, 2, x=100, y=200, 12)
+func(1, 2, x=100, y=200)
+print('-'*80)
+
 
 
 
