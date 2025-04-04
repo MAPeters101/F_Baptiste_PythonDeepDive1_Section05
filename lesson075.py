297f9c7427878d3245d5a870d93371b9036d4d61
Mark Peters <mapeters101@earthlink.net>
2025-04-03 23:58:57 -0400

    Work on lesson075 from 4:30 to 6:15.

--- lesson075.py
+++ lesson075.py
@@ -15,6 +15,22 @@ func(1,2,3,'x','y','z')
 func(1,4,3,'x','y','z')
 print('-'*80)
 
+func(1,c=5)
+#func(1,c=5,'x','y')
+print('='*80)
 
+def func(a, b=2, *args, c=3, d):
+    print(a)
+    print(b)
+    print(args)
+    print(c)
+    print(d)
+func(10, 20, 'x','y','z', c=4, d=1)
+print()
+func(10, 20, 'x','y','z', d=10)
+print('-'*80)
+
+#func(1, 'x', 'y', 'z', b=4, d=10)
+func(1, 'x', 'y', 'z', d=10)
 
 
