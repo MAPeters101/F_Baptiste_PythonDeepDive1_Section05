e068b90a593e32c94b476633586684aff04ad0a3
Mark Peters <mapeters101@earthlink.net>
2025-04-05 00:12:31 -0400

    Work on lesson076 from 5:20 to 6:45.

--- lesson076.py
+++ lesson076.py
@@ -1,22 +1,29 @@
 import time
 
-def time_it(fn, * args, **kwargs):
+def time_it(fn, *args, **kwargs):
     print(args, kwargs)
 
 time_it(print, 1, 2, 3, sep=' - ', end=' ***')
 print('-'*80)
 
-def time_it(fn, * args, **kwargs):
+def time_it(fn, *args, **kwargs):
     fn(args, kwargs)
 
 time_it(print, 1, 2, 3, sep=' - ', end=' ***')
 print((1,2,3), {'sep': ' - ', 'end': ' ***'})
 print('-'*80)
 
-def time_it(fn, * args, **kwargs):
+def time_it(fn, *args, **kwargs):
     fn(*args, **kwargs)
 
 time_it(print, 1, 2, 3, sep=' - ', end=' ***')
 print()
-print('-'*80)
+print('.'*80)
+
+def time_it(fn, *args, rep=1, **kwargs):
+    for i in range(rep):
+       fn(*args, **kwargs)
+
+time_it(print, 1, 2, 3, sep=' - ', end=' ***\n', rep=5)
+print('='*80)
 
