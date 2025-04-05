1cb63b3b053d5bb132546dd063fd784524d07285
Mark Peters <mapeters101@earthlink.net>
2025-04-05 00:15:00 -0400

    Work on lesson076 from 6:45 to 7:35.

--- lesson076.py
+++ lesson076.py
@@ -27,3 +27,13 @@ def time_it(fn, *args, rep=1, **kwargs):
 time_it(print, 1, 2, 3, sep=' - ', end=' ***\n', rep=5)
 print('='*80)
 
+def time_it(fn, *args, rep=1, **kwargs):
+    start = time.perf_counter()
+    for i in range(rep):
+       fn(*args, **kwargs)
+    end = time.perf_counter()
+    return (end - start) / rep
+
+print(time_it(print, 1, 2, 3, sep=' - ', end=' ***\n', rep=5))
+print('='*80)
+
