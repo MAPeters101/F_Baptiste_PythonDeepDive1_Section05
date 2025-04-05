77f4cac96cd781146e8801a69beb41c39e2cd03d
Mark Peters <mapeters101@earthlink.net>
2025-04-05 00:07:40 -0400

    Work on lesson076 from 2:40 to 4:30.

--- lesson076.py
+++ lesson076.py
@@ -4,5 +4,12 @@ def time_it(fn, * args, **kwargs):
     print(args, kwargs)
 
 time_it(print, 1, 2, 3, sep=' - ', end=' ***')
+print('-'*80)
 
+def time_it(fn, * args, **kwargs):
+    fn(args, kwargs)
+
+time_it(print, 1, 2, 3, sep=' - ', end=' ***')
+print((1,2,3), {'sep': ' - ', 'end': ' ***'})
+print('-'*80)
 
